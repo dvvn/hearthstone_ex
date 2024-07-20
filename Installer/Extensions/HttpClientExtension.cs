6fa1fe1a5cc675a5d74fa9b2007b5dea933d80a7
dvvn <dvvn@ya.ru>
2024-07-20 18:04:28 +0300

    Remove useless DirectoryInfo

--- Installer/Extensions/HttpClientExtension.cs
+++ Installer/Extensions/HttpClientExtension.cs
@@ -7,38 +7,42 @@ namespace Installer.Extensions;
 
 internal static class HttpClientExtension
 {
-    public static async Task<HttpResponseMessage> SendHttpRequest(this HttpClient client, Uri url)
-    {
-        using var request = new HttpRequestMessage(HttpMethod.Head, url);
-        return await client.SendAsync(request);
-    }
+	public static async Task<HttpResponseMessage> SendHttpRequest(this HttpClient client, Uri url)
+	{
+		using var request = new HttpRequestMessage(HttpMethod.Head, url);
+		return await client.SendAsync(request);
+	}
 
-    public static async Task<bool> FileExist(this HttpClient client, Uri url)
-    {
-        using var response = await client.SendHttpRequest(url);
-        return response.StatusCode == HttpStatusCode.OK;
-    }
+	public static async Task<bool> FileExist(this HttpClient client, Uri url)
+	{
+		using var response = await client.SendHttpRequest(url);
+		return response.StatusCode == HttpStatusCode.OK;
+	}
 
-    public static async Task<long> GetFileSize(this HttpClient client, Uri url)
-    {
-        using var response = await client.SendHttpRequest(url);
-        response.EnsureSuccessStatusCode();
-        return response.Content.Headers.ContentLength.Value;
-    }
+	public static async Task<long> GetFileSize(this HttpClient client, Uri url)
+	{
+		using var response = await client.SendHttpRequest(url);
+		response.EnsureSuccessStatusCode( );
+		return response.Content.Headers.ContentLength.Value;
+	}
 
-    public static async Task Download(this HttpClient client, UnstripHelper unstripHelper)
-    {
-        var directory = unstripHelper.ToString();
-        var directoryInfo = new DirectoryInfo(directory);
-        if (directoryInfo.Exists && directoryInfo.EnumerateFiles().Any())
-            return;
+	public static async Task Download(this HttpClient client, UnstripHelper unstripHelper)
+	{
+		var directory = unstripHelper.ToString( );
 
-        var archive = new ZipArchive(await client.GetStreamAsync(unstripHelper.GetUrl()));
-#if DEBUG
-        foreach (var e in archive.Entries)
-            Debug.Assert(e.Name == e.FullName);
-#endif
-        directoryInfo.Create();
-        await Task.WhenAll(archive.Entries.Select(e => e.WriteTo(Path.Combine(directory, e.Name))));
-    }
+		if (!Directory.Exists(directory))
+			Directory.CreateDirectory(directory);
+		else if (Directory.EnumerateFiles(directory).Any( ))
+			return;
+
+		using var archive = new ZipArchive(await client.GetStreamAsync(unstripHelper.GetUrl( )));
+
+		await Task.WhenAll(
+			archive.Entries.Select(
+				e =>
+				{
+					Debug.Assert(e.Name == e.FullName);
+					return e.WriteTo(Path.Combine(directory, e.Name));
+				}));
+	}
 }
